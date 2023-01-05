fca35c1eb79083f8d6b42b1ac97cd9012e928289
PAVAN903 <navap903@gmail.com>
2023-01-05 16:04:58 +0530

    check max float value

---
+++ Generics.java
@@ -0,0 +1,56 @@
+package com.generics;
+
+import java.util.Scanner;
+
+public class Generics <T extends Comparable<T>> {
+	T a;
+	T b;
+	T c;
+	
+	public Generics(T a, T b, T c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+	public T Max() {
+	return Generics.Max(a,b,c);
+	}
+	public static <T extends Comparable <T>>T Max(T a,T b,T c){
+		T Max=a;    //deafult a is max value
+		
+		if(b.compareTo(Max)>0) {  //max value b
+			Max=b;
+		}
+		if(c.compareTo(Max)>0) {  //max value c
+			Max=c;
+		}
+		printMax(Max);
+		return Max;
+	    }	
+	public static <T> void printMax(T Max) {
+		System.out.println("maximum float value is:: "+Max);  //to print maximum value
+		};
+		
+    public static void main(String[] args) {
+    	// int a;int b;int c;
+    	 //Scanner sc=new Scanner(System.in);
+    	 //System.out.println("enter first integer value");
+    	 // a=sc.nextInt();
+    	  //System.out.println("enter second  integer value");
+    	  //b=sc.nextInt();
+    	  //System.out.println("enter third integer value");
+    	  //c=sc.nextInt();
+    	
+    	  float a;float b;float c;
+    	  Scanner sc=new Scanner(System.in);
+    	  System.out.println("enter first float value");
+    	  a=sc.nextFloat();
+    	  System.out.println("enter second float value");
+    	  b=sc.nextFloat();
+    	  System.out.println("enter third float value");
+    	  c=sc.nextFloat();
+    	  
+		  
+    	  new Generics( a, b, c).Max();  //comparing with 3 int max value
+    	  }
+          }
